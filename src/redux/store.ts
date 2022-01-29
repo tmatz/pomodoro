3ee255dd914f182ffae6d9b66e15d2bc08b12ed0
MATSUDA Takashi <tmatz.dev@gmail.com>
2022-01-29 14:57:21 +0900

    feat: add epic middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { createEpicMiddleware } from "redux-observable";
+import epics from "~/epics";
 import * as pomodoro from "./modules/pomodoro";
+
+const epicMiddleware = createEpicMiddleware();
 
 export const store = configureStore({
   reducer: {
     pomodoro: pomodoro.slice.reducer,
   },
+  middleware: (getDefaultMiddlewares) => [
+    ...getDefaultMiddlewares({}),
+    epicMiddleware,
+  ],
 });
+
+epicMiddleware.run(epics);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
